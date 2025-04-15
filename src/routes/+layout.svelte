4ddc3c9edf69359b7ed6b9c38ccffc05c1df9fb9
akashic.zhang <2095138755@qq.com>
2025-04-15 10:32:19 +0800

    test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script>
   let { children } = $props();
-  import "../app.css";
+  import "/src/app.css";
 </script>
 
 {@render children()}
+
+
+<style lang="postcss">
+  @reference "tailwindcss";
+  :global(html) {
+    background-color: theme(--color-gray-100);
+  }
+</style>
