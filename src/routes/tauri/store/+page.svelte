c17940441e383dd496cea06a52b42c31b1cbd8d9
akashic.zhang <2095138755@qq.com>
2025-04-16 16:47:57 +0800

    add store

--- src/routes/tauri/store/+page.svelte
+++ src/routes/tauri/store/+page.svelte
@@ -1,1 +1,75 @@
-<h2>Store</h2>
+<script>
+  import { LazyStore } from "@tauri-apps/plugin-store";
+  import {
+    A,
+    CloseButton,
+    Table,
+    TableBody,
+    TableBodyCell,
+    TableBodyRow,
+    TableHead,
+    TableHeadCell,
+    FloatingLabelInput,
+  } from "flowbite-svelte";
+
+  const store = new LazyStore("demo.json");
+
+  async function getEntries() {
+    return await store.entries();
+  }
+
+  // let items = $state([]);
+  let promise = $state(getEntries());
+
+  let key = $state("");
+  let value = $state("");
+
+  async function handleInsert() {
+    await store.set(key, value);
+    promise = getEntries();
+  }
+
+  async function handleDelete(key) {
+    await store.delete(key);
+    promise = getEntries();
+  }
+</script>
+
+{#await promise then items}
+  <Table shadow {items}>
+    <caption
+      class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800"
+    >
+      持久化的键值存储
+    </caption>
+    <TableHead>
+      <TableHeadCell>Key</TableHeadCell>
+      <TableHeadCell>Value</TableHeadCell>
+      <TableHeadCell>
+        <span class="sr-only">操作</span>
+      </TableHeadCell>
+    </TableHead>
+    <TableBody tableBodyClass="divide-y">
+      <TableBodyRow>
+        <TableBodyCell>
+          <FloatingLabelInput size="small" bind:value={key}>
+            键
+          </FloatingLabelInput>
+        </TableBodyCell>
+        <TableBodyCell>
+          <FloatingLabelInput size="small" bind:value>值</FloatingLabelInput>
+        </TableBodyCell>
+        <TableBodyCell>
+          <A onclick={handleInsert}>插入</A>
+        </TableBodyCell>
+      </TableBodyRow>
+      <TableBodyRow slot="row" let:item>
+        <TableBodyCell>{item[0]}</TableBodyCell>
+        <TableBodyCell>{item[1]}</TableBodyCell>
+        <TableBodyCell>
+          <CloseButton on:click={handleDelete(item[0])} />
+        </TableBodyCell>
+      </TableBodyRow>
+    </TableBody>
+  </Table>
+{/await}
