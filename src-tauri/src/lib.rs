d5685cf4c6db4348d153740533f29e529d48cc84
akashic.zhang <2095138755@qq.com>
2025-04-17 16:42:56 +0800

    add sql

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri_plugin_sql::{Migration, MigrationKind};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -6,7 +8,31 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let migrations = vec![Migration {
+        version: 1,
+        description: "create_initial_tables",
+        sql: "
+DROP TABLE IF EXISTS 'users';
+CREATE TABLE 'users' (
+  'id' INTEGER DEFAULT NULL COLLATE BINARY PRIMARY KEY AUTOINCREMENT,
+  'name' TEXT NOT NULL,
+  'age' INTEGER NOT NULL
+);
+INSERT INTO 'users' VALUES (1, 'tom', 18);
+UPDATE 'sqlite_sequence' SET seq = 1 WHERE name = 'users';
+CREATE INDEX 'index_age'
+ON 'users' (
+  'age' ASC
+);",
+        kind: MigrationKind::Up,
+    }];
+
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_sql::Builder::new()
+                .add_migrations("sqlite:test.db", migrations)
+                .build(),
+        )
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_http::init())
--- src/routes/tauri/sql/+page.svelte
+++ src/routes/tauri/sql/+page.svelte
@@ -1,1 +1,78 @@
-<h2>SQL</h2>
+<script>
+  // https://github.com/tauri-apps/plugins-workspace/blob/v2/plugins/sql/guest-js/index.ts
+
+  import Database from '@tauri-apps/plugin-sql';
+  import {
+    Button,
+    Card,
+    Hr,
+    Table,
+    TableBody,
+    TableBodyCell,
+    TableBodyRow,
+    TableHead,
+    TableHeadCell,
+    FloatingLabelInput,
+  } from "flowbite-svelte";
+
+  async function handleQuery() {
+    const db = await Database.load("sqlite:test.db");
+    const records = await db.select("SELECT * FROM users");
+    await db.close();
+    return records;
+  }
+
+  let name = $state("");
+  let age = $state("");
+
+  async function handleSubmit() {
+    alert(name + " " + age);
+  }
+
+  let promise = handleQuery();
+</script>
+
+<div class="flex flex-col justify-center items-center pt-[1vh]">
+  <Card>
+    <form class="flex flex-col space-y-4" onsubmit={handleSubmit}>
+      <h3 class="text-lg font-medium text-gray-900 dark:text-white">用户</h3>
+      <FloatingLabelInput style="outlined" size="small" bind:value={name}
+        >姓名</FloatingLabelInput
+      >
+      <FloatingLabelInput
+        style="outlined"
+        size="small"
+        bind:value={age}
+        type="number"
+        min="0">年龄</FloatingLabelInput
+      >
+      <Button type="submit">插入</Button>
+    </form>
+  </Card>
+
+  {#await promise then items}
+    <br />
+    <Table noborder striped {items}>
+      <caption
+        class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800"
+      >
+        数据库
+        <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">
+          users表
+        </p>
+      </caption>
+      <TableHead>
+        <TableHeadCell>ID</TableHeadCell>
+        <TableHeadCell>Name</TableHeadCell>
+        <TableHeadCell>Age</TableHeadCell>
+      </TableHead>
+      <TableBody tableBodyClass="divide-y">
+        <TableBodyRow slot="row" let:item>
+          <TableBodyCell>{item.id}</TableBodyCell>
+          <TableBodyCell>{item.name}</TableBodyCell>
+          <TableBodyCell>{item.age}</TableBodyCell>
+        </TableBodyRow>
+      </TableBody>
+    </Table>
+  {/await}
+</div>
